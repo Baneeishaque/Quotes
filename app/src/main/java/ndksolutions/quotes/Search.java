2dae23c9f547f8423f37b5a2e281d452ddafe339
baneeishaque <baneeishaque@gmail.com>
2018-06-16 10:49:10 +0530

    Initial commit

---
+++ app/src/main/java/ndksolutions/quotes/Search.java
@@ -0,0 +1,18 @@
+package ndksolutions.quotes;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Search extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+    }
+
+
+}
